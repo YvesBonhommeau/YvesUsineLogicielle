535edecccef82d1a943f7632572c71754566feed
YvesBonhommeau <yves.bonhommeau@free.fr>
2022-06-30 10:41:05 +0200

    Remise en conformité

--- src/main/java/com/starterkit/springboot/brs/BusReservationSystemApplication.java
+++ src/main/java/com/starterkit/springboot/brs/BusReservationSystemApplication.java
@@ -21,8 +21,7 @@ import java.util.Set;
 public class BusReservationSystemApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BusReservationSystemApplication.class, args)
-		//SpringApplication.run(BusReservationSystemApplication.class, args);
+        SpringApplication.run(BusReservationSystemApplication.class, args);
  }
 
     @Bean
