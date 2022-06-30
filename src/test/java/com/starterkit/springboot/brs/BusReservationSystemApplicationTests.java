324916419c8d0b6cb573e5734653832f317d8b7a
YvesBonhommeau <108268285+YvesBonhommeau@users.noreply.github.com>
2022-06-30 10:16:16 +0100

    Update BusReservationSystemApplicationTests.java

--- src/test/java/com/starterkit/springboot/brs/BusReservationSystemApplicationTests.java
+++ src/test/java/com/starterkit/springboot/brs/BusReservationSystemApplicationTests.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -11,6 +12,10 @@ public class BusReservationSystemApplicationTests {
 
     @Test
     public void contextLoads() {
+        int a=5; 
+        int b=10;
+        
+        assertEquals(a, b);
     }
 
 }
